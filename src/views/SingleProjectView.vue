228235b9343d852abb38ba762cb53c93637945cd
Elise Carey <aelisecarey@gmail.com>
2024-11-23 09:15:53 -0500

    add dynamic project views

---
+++ src/views/SingleProjectView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+</script>
+
+<template>
+  <div>
+    <h1>Project {{ route.params?.id }}</h1>
+    <div>
+      <RouterLink to="/">Home</RouterLink> | <RouterLink to="/projects">Projects</RouterLink>
+    </div>
+  </div>
+</template>
